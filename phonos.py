e82988e37543c9000a560e04c43cc9cb752fe09c
Jason Rosenman <jvrosenman@gmail.com>
2018-05-22 18:19:23 -0400

    Added playback functionality, using URIs stored in a config file.

--- phonos.py
+++ phonos.py
@@ -2,17 +2,25 @@ import rotary
 import soco
 import time
 
+try:
+    import config as cfg
+except ModuleNotFoundError:
+    import sys
+    print("You need a config.py!")
+    sys.exit(-1)
+
 zp = soco.discovery.any_soco()
 coord = zp.group.coordinator
 
 def cb(value):
+    coord.partymode()
     if value == 10:
         print("Pause!")
         coord.pause()
-    if value == 1:
-        print("Play!")
-        coord.play()
+    else:
+        coord.play_uri(cfg.uris[value])
 
+# These are BeagleBoard-formatted pins.
 phone = rotary.Rotary("P8_9", "P8_10", cb)
 
 phone.start()
